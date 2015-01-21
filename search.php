a8e2d53a531e34c2d7d2ee3ce28a3841ce589198
clay-to-n <cbrand@usc.edu>
2015-01-20 19:42:01 -0800

    Added search page with no functionality

---
+++ search.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DVD Search</title>
+  </head>
+  
+  <form action="search.php" method="get">
+    Search DVD's: <input type="text" name="artist" placeholder="Die Hard">
+    <input type="submit" value="search">
+  </form>
+  
+</html>
